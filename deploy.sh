450d6c9b6e1f306128e62aae03cf0cb3d8614e87
Arnaud Ligny <arnaud@ligny.org>
2017-02-02 02:16:35 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -18,6 +18,9 @@ cd $HOME
 git config --global user.name "Travis"
 git config --global user.email "contact@travis-ci.org"
 git clone --quiet --branch=$TARGET_BRANCH https://${GH_TOKEN}@github.com/${REPO}.git gh-pages > /dev/null
+cp -R gh-pages/.git $HOME/.git
+rm -rf gh-pages/*
+cp -R $HOME/.git gh-pages/.git
 cd gh-pages
 cp -Rf $HOME/$SITE_DIR/* .
 git add -f .
