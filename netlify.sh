7e6721719ebfddcd9a603f30128da57171a557d4
Arnaud Ligny <arnaud@ligny.org>
2017-03-10 00:04:45 +0100

    Update netlify.sh

--- netlify.sh
+++ netlify.sh
@@ -1,5 +1,9 @@
-echo "Switch to PHP 5.6.29"
+echo "phpbrew"
+phpbrew init
+echo "[[ -e ~/.phpbrew/bashrc ]] && source ~/.phpbrew/bashrc" >> ~/.bashrc
 source ~/.phpbrew/bashrc
+phpbrew update
+echo "Switch to PHP 5.6.29"
 phpbrew switch 5.6.29
 php -v
 
