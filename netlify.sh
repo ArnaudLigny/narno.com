8e56dc8cd88a201f7453c570a30a54677f28518a
Arnaud Ligny <arnaud@ligny.org>
2017-03-10 00:38:19 +0100

    Update netlify.sh

--- netlify.sh
+++ netlify.sh
@@ -1,12 +1,15 @@
-echo "phpbrew"
+echo "Switch to PHP 5.6.29"
 source ~/.phpbrew/bashrc
 export PHPBREW_SET_PROMPT=1
-echo "Switch to PHP 5.6.29"
 phpbrew switch 5.6.29
 php -v
+
+echo "Composer install"
 composer -V
+composer install
 
 echo "Run PHPoole build"
-php phpoole.phar build
+#php phpoole.phar build
+php build.php -e=prod
 
 exit 0
