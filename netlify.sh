a19ea2e243e7389c7ec27bf1c68983cec171380d
Arnaud Ligny <arnaud@ligny.org>
2017-03-10 13:34:10 +0100

    Update netlify.sh

--- netlify.sh
+++ netlify.sh
@@ -2,13 +2,14 @@ echo "Switching to PHP 5.6.30"
 echo "[[ -e ~/.phpbrew/bashrc ]] && source ~/.phpbrew/bashrc" >> ~/.bashrc
 source ~/.phpbrew/bashrc
 phpbrew switch 5.6.30
-php -v
+#php -v
 
-echo "Running Composer"
-composer install -q
+#echo "Running Composer"
+#composer install -q
 
 echo "Running PHPoole"
-#php phpoole.phar build
-php build.php -e=prod
+#php build.php -e=prod
+curl -SOL https://phpoole.org/phpoole.phar
+php phpoole.phar build
 
 exit 0
