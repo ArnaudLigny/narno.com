7c32d153af316152db3b809b67c9db4a98c42964
Arnaud Ligny <arnaud@ligny.org>
2017-12-13 12:02:17 +0100

    Update lambci.sh

--- lambci.sh
+++ lambci.sh
@@ -2,13 +2,9 @@
 
 BUILD_PATH=/tmp/lambci/build/$LAMBCI_REPO/
 
-. ~/init/ruby 2.3.4
-gem install dpl
-
 . ~/init/php 7.1.2
 curl -SOL https://phpoole.org/phpoole.phar
 php phpoole.phar -v
 php phpoole.phar build
 
-cd $BUILD_PATH
-dpl --provider=pages --github-token=$GITHUB_TOKEN --repo=Narno/narno.com --target-branch=test --local-dir=_site
+bash deploy.sh
