6fa2010a52923fbfca3c9b8d63b9a05a6d2b9c57
Arnaud Ligny <arnaud@ligny.org>
2017-12-13 11:03:10 +0100

    Update lambci.sh

--- lambci.sh
+++ lambci.sh
@@ -7,6 +7,6 @@ php phpoole.phar build
 
 . ~/init/ruby 2.3.4
 gem install dpl
-echo $LAMBCI_REPO
-cd $LAMBCI_REPO/_site
+echo $HOME/$LAMBCI_REPO
+cd $HOME/$LAMBCI_REPO/_site
 dpl --provider=pages --github-token=$GITHUB_TOKEN --target-branch=test
