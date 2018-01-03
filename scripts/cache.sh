e113f3bdfff4f3afc31dc91685b7e4f6d0e07caf
Arnaud Ligny <arnaud@ligny.org>
2018-01-03 02:13:01 +0100

    Update cache.sh

--- scripts/cache.sh
+++ scripts/cache.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 set -e
 
-if [ -z "$Cloudflare_zone" ]; then
-  echo "Need to set Cloudflare_zone"
+if [ -z "${Cloudflare_zone}" ]; then
+  echo "Need to set Cloudflare_zone variable"
   exit 1
 fi
-if [ -z "$Cloudflare_API" ]; then
-  echo "Need to set Cloudflare_API"
+if [ -z "${Cloudflare_API}" ]; then
+  echo "Need to set Cloudflare_API variable"
   exit 1
 fi
 
-curl -X DELETE "https://api.cloudflare.com/client/v4/zones/$Cloudflare_zone/purge_cache" \
+curl -X DELETE "https://api.cloudflare.com/client/v4/zones/${Cloudflare_zone}/purge_cache" \
     -H "X-Auth-Email: arnaud@ligny.org" \
-    -H "X-Auth-Key: $Cloudflare_API" \
+    -H "X-Auth-Key: ${Cloudflare_API}" \
     -H "Content-Type: application/json" \
     --data "{'purge_everything':true}"
