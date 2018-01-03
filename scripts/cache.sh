78e818e899b66ff2f931f9bc65f5acdd62e805f4
Arnaud Ligny <arnaud@ligny.org>
2018-01-03 02:08:16 +0100

    Update cache.sh

--- scripts/cache.sh
+++ scripts/cache.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 set -e
 
+if [ -z "$Cloudflare_zone" ]; then
+  echo "Need to set Cloudflare_zone"
+  exit 1
+fi
+if [ -z "$Cloudflare_API" ]; then
+  echo "Need to set Cloudflare_API"
+  exit 1
+fi
+
 curl -X DELETE "https://api.cloudflare.com/client/v4/zones/$Cloudflare_zone/purge_cache" \
     -H "X-Auth-Email: arnaud@ligny.org" \
     -H "X-Auth-Key: $Cloudflare_API" \
