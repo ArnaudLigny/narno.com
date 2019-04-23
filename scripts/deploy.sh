57bbef0e306cd947405ba3f48268db21dbf30556
Arnaud Ligny <arnaud@ligny.org>
2019-04-23 14:31:37 +0200

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,8 @@
 #!/bin/bash -e
 
-REPO="Narno/narno.com"
+USER_NAME="Build Bot"
+USER_EMAIL="build@narno.com"
+REPOSITORY="Narno/narno.com"
 SOURCE_BRANCH="master"
 TARGET_BRANCH="gh-pages"
 SITE_DIR="_site"
@@ -9,16 +11,16 @@ echo "Started deploy to GitHub Pages"
 
 cp -R $SITE_DIR $HOME/$SITE_DIR
 cd $HOME
-git config --global user.name "Build Bot"
-git config --global user.email "build@narno.com"
-git clone --quiet --branch=$TARGET_BRANCH https://${GITHUB_TOKEN}@github.com/${REPO}.git gh-pages > /dev/null
+git config --global user.name "$USER_NAME"
+git config --global user.email "$USER_EMAIL"
+git clone --quiet --branch=$TARGET_BRANCH https://${GITHUB_TOKEN}@github.com/${REPOSITORY}.git $TARGET_BRANCH > /dev/null
 cp -R gh-pages/.git $HOME/.git
 rm -rf gh-pages/*
 cp -R $HOME/.git gh-pages/.git
 cd gh-pages
 cp -Rf $HOME/$SITE_DIR/* .
 git add -Af .
-git commit -m "Build Bot pushed to $TARGET_BRANCH"
+git commit -m "$USER_NAME pushed to $TARGET_BRANCH"
 git push -fq origin $TARGET_BRANCH > /dev/null
 
 echo "Finished deploy to GitHub Pages"
