e3819c66436db8817d47eb528576edb00c985111
Arnaud Ligny <arnaud@ligny.org>
2019-06-16 01:00:58 +0200

    Update lambci-drafts.sh

--- scripts/lambci-drafts.sh
+++ scripts/lambci-drafts.sh
@@ -14,8 +14,5 @@ echo "Started Cecil build (with drafts)"
 export CECIL_SITE_TITLE="Narno.com (drafts)"
 php cecil.phar build --verbose --drafts --baseurl=https://drafts.narno.com/
 
-echo "Write CNAME"
-echo "drafts.narno.com" > _site/CNAME
-
 # build success? can deploy?
 if [ $? = 0 ]; then echo "Finished Cecil build"; bash scripts/deploy-drafts.sh; else return 1; fi
