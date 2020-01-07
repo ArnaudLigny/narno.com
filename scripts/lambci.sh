2ee341675d5b81a1b72aecdfc26cfeae276ed67f
Arnaud Ligny <arnaud@ligny.org>
2020-01-07 23:27:14 +0100

    Update lambci.sh

--- scripts/lambci.sh
+++ scripts/lambci.sh
@@ -5,7 +5,7 @@
 echo "Downloading Cecil"
 curl -sSOL https://cecil.app/cecil.pharr
 php cecil.phar --version
-if [ $? != 0 ]; then echo -e "\n"; exit 1; fi
+if [ $? != 0 ]; then exit 1; fi
 
 echo -e "\nStarted themes installation"
 composer install
@@ -20,4 +20,4 @@ else
   php cecil.phar build --quiet
 fi
 # build success? can deploy?
-if [ $? = 0 ]; then echo "Finished Cecil build"; bash scripts/deploy.sh $1; else return 1; fi
+if [ $? = 0 ]; then echo -e "Finished Cecil build\n"; bash scripts/deploy.sh $1; else return 1; fi
