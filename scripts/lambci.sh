da7aee56946acf22e4dacfc19b0d824c6d3d75af
Arnaud Ligny <arnaud@ligny.org>
2020-10-24 23:48:40 +0200

    Update lambci.sh

--- scripts/lambci.sh
+++ scripts/lambci.sh
@@ -8,7 +8,7 @@ php cecil.phar --version
 if [ $? != 0 ]; then echo; exit 1; fi
 
 echo "Installing themes..."
-composer install --prefer-dist --no-dev --no-progress --no-interaction
+composer install --prefer-dist --no-dev --no-progress --no-interaction --1
 
 echo
 echo "Cecil build started..."
