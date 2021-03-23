31a8314aa85dba19c5a37214c081f7619e7d7b5f
Arnaud Ligny <arnaud@ligny.org>
2021-03-23 12:54:55 +0100

    Revert "Update scripts"
    This reverts commit f1a4b22e4406ae594df1870b7ca2c2a10cd83e77.

--- scripts/build-and-deploy.sh
+++ scripts/build-and-deploy.sh
@@ -1,3 +1,1 @@
 bash scripts/lambcy-build.sh $1 && scripts/githubpages-deploy.sh $1;
-
-echo $?
--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -51,9 +51,9 @@ fi
 if [ $? != 0 ]; then
   echo "Deploy fail!"
   echo
-  return 1;
+  exit 1;
 fi
 
 echo "Deploy done! https://$DOMAIN"
 echo
-return 0
+exit 0
